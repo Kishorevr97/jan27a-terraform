72bc0fee0b8653483f6247b3e6acbf006474a97d
Kishorevr97 <107535279+Kishorevr97@users.noreply.github.com>
2025-01-28 10:25:03 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -11,6 +11,12 @@ variable "ami" {
 variable "instance_type" {
         description = "instance type of the server"
 
+validation {
+       condition = contains(["t2.micro", "t3.micro"], var.instance_type)
+       error_message = "instance type must be either 't2.micro' or 't3.micro'."
+
+      }
+
 }
 
 variable "db_engine" {
